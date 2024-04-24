df275606713bbc7daf883548bd5f8f6fb24ea79d
Maximilian Muecke <muecke.maximilian@gmail.com>
2024-04-24 18:03:14 +0200

    tests: add fixture and test for fetching latest relevant url

--- tests/testthat/test-ifo.R
+++ tests/testthat/test-ifo.R
@@ -1,3 +1,12 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("ifo_url() returns expected result", {
+  local_mocked_bindings(
+    read_html = function(x) {
+      xml2::read_html(test_path("fixtures", "ifo-time-series.html"))
+    }
+  )
+  expected <- "https://www.ifo.de/sites/default/files/secure/timeseries/gsk-e-202404.xlsx" # nolint
+  expect_identical(ifo_url("germany"), expected)
+  expect_identical(ifo_url("sectors"), expected)
+  types <- c("germany", "sectors", "eastern", "saxony", "export", "employment", "export_climate", "import_climate") # nolint
+  lapply(types, \(type) expect_length(ifo_url(type), 1L))
 })
